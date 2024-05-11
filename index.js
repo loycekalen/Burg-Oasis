c7d123c59982c8b5ebc9cb5581765f24a027f9fd
loycekalen <166914558+loycekalen@users.noreply.github.com>
2024-05-11 14:38:36 +0300

    added css and js into index.html

--- index.js
+++
@@ -1,8 +0,0 @@
-function subscribeButton(event) {
-  event.preventDefault();
-  let paragraph = document.querySelector("h6");
-  paragraph.innerHTML = "Thank You for Subscribing!🤞";
-}
-
-let subscribe = document.querySelector("#sign-up");
-subscribe.addEventListener("click", subscribeButton);
